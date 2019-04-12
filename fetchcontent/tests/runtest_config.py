ece66acdb6ed8fcc246548035961f1123edf3943
Stig Rune Jensen <stig.r.jensen@uit.no>
2019-04-12 14:13:02 +0200

    Move parselglossy run from runtest_config to test

--- fetchcontent/tests/runtest_config.py
+++ fetchcontent/tests/runtest_config.py
@@ -1,7 +1,5 @@
 from os import path
 from sys import platform
-import subprocess
-
 
 def configure(options, input_files, extra_args):
     """
@@ -9,27 +7,13 @@ def configure(options, input_files, extra_args):
     at runtime for code specific launch command and file naming.
     """
 
-    fr_file = 'pi.json'
-    tp_file = '../../../src/template.yml'
-    grammar = 'getkw'
-
-    # Running parselglossy CLI: pi.inp -> pi.json
-    parsel_command = []
-    parsel_command.append('parselglossy')
-    parsel_command.append('parse')
-    parsel_command.append('--outfile=' + fr_file)
-    parsel_command.append('--template=' + tp_file)
-    parsel_command.append('--grammar=' + grammar)
-    parsel_command.append(input_files)
-    subprocess.call(parsel_command)
-
     # Running C++ executable
     launcher = 'pi'
     launcher_full_path = path.normpath(path.join(options.binary_dir, launcher))
 
     command = []
     command.append(launcher_full_path)
-    command.append(fr_file)
+    command.append(input_files)
 
     if extra_args is not None:
         command.append(extra_args)
